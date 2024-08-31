4f81fbcf36f506a801079e0b1a4de4618e35b463
Maultasche <peter@example.com>
2024-08-31 19:10:29 +0200

    changed festival checking into a standalone script

---
+++ WaifuGame Festival Checker.user.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name         WaifuGame Festival Checker
+// @description  WG Tweaks by Maultasche
+// @match        https://waifugame.com*
+// @namespace    https://github.com/maultasche92/WaifuGame-Tweaks
+// @author       maultasche92
+// @version      1.0
+// @updateURL    https://github.com/maultasche92/WaifuGame-Tweaks/blob/main/WaifuGame%20Festival%20Checker.user.js
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=waifugame.com
+// @grant        unsafeWindow
+// ==/UserScript==
+
+// Configurable part begins
+// Add the item name (exact name) into the brackets, surrounded by quotes and separated by comma
+var festivalChecks = ["Tier 3 Day-Pass"];
+// Configurable part ends
+
+
+$.get(location.origin + '/festival').then((htmlString) => {
+    const items = [];
+    let match;
+    const regex = /showInfoModal\(\"(.*?)\", itemDescription\);/g;
+
+    while ((match = regex.exec(htmlString)) !== null) {
+        items.push(match[1]);
+    }
+
+    var match2 = null;
+    items.forEach((item) => {
+        if (!match2 && festivalChecks.indexOf(item) > -1) {
+            match2 = item;
+            document.querySelector("#menu-main a[href$=festival]").insertAdjacentHTML("beforeend", '<span class="badge bg-highlight font-10">!!!</span>');
+        }
+    });
+});
--- WaifuGame Swiper Tweaks.user.js
+++ WaifuGame Swiper Tweaks.user.js
@@ -1,20 +1,14 @@
 // ==UserScript==
 // @name         WaifuGame Swiper Tweaks
-// @description  Swiping with keyboard arrow keys,changing formation quickly,check festival for items
+// @description  WG Tweaks by Maultasche
 // @match        https://waifugame.com/swiper*
 // @namespace    https://github.com/maultasche92/WaifuGame-Tweaks
 // @author       maultasche92
-// @version      1.11
+// @version      1.2
 // @updateURL    https://github.com/maultasche92/WaifuGame-Tweaks/blob/main/WaifuGame%20Swiper%20Tweaks.user.js
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=waifugame.com
 // @grant        unsafeWindow
 // ==/UserScript==
-
-// Configurable part begins
-// Add the item name (exact name) into the brackets, surrounded by quotes and separated by comma
-var festivalChecks = ["Tier 3 Day-Pass"];
-// Configurable part ends
-
 var charmId = "", swipingId = "", token = "", switchcount = 1;
 
 document.addEventListener('keydown', function (event) {
@@ -79,31 +73,11 @@ function charming() {
 function switchteam() {
     switchcount = !switchcount; switchcount ? swiping() : charming()
 };
-function checkFestival() {
-    $.get(location.origin + '/festival').then((htmlString) => {
-        const items = [];
-        let match;
-        const regex = /showInfoModal\(\"(.*?)\", itemDescription\);/g;
-
-        while ((match = regex.exec(htmlString)) !== null) {
-            items.push(match[1]);
-        }
-
-        var match2 = null;
-        items.forEach((item) => {
-            if (!match2 && festivalChecks.indexOf(item) > -1) {
-                match2 = item;
-                document.querySelector("#menu-main a[href$=festival]").insertAdjacentHTML("beforeend", '<span class="badge bg-highlight font-10">!!!</span>');
-            }
-        });
-    });
-}
 
 document.getElementsByClassName("tinder--buttons")[0].insertAdjacentHTML('afterbegin', `<button title="Arrow Keys Left/Right=Swiping, Up=Switch Team, Down=show stock of card" id="partyswitcher" style="text-overflow:ellipsis;overflow: hidden;white-space: nowrap;">...</button>`);
 document.getElementById('partyswitcher').addEventListener('click', switchteam);
 
 window.setTimeout(() => {
-    checkFestival();
     fetchTeams();
     window.setInterval(() => {
         if (document.querySelector(".tinder--cards strong").innerText.includes("Just for you")
