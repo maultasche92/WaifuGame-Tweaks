6ca21f26d564d889a1e8678755364bbed9e8d36f
Maultasche <peter@example.com>
2024-11-28 13:11:31 +0100

    Show Top Simp

--- WaifuGame Card Portfolio Tweaks.user.js
+++ WaifuGame Card Portfolio Tweaks.user.js
@@ -5,7 +5,7 @@
 // @match        https://waifugame.com/cards?*
 // @namespace    https://github.com/maultasche92/WaifuGame-Tweaks
 // @author       maultasche92
-// @version      1.6
+// @version      1.7
 // @updateURL    https://github.com/maultasche92/WaifuGame-Tweaks/raw/main/WaifuGame%20Card%20Portfolio%20Tweaks.user.js
 // @downloadURL  https://github.com/maultasche92/WaifuGame-Tweaks/raw/main/WaifuGame%20Card%20Portfolio%20Tweaks.user.js
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=waifugame.com
@@ -55,6 +55,42 @@
         if (!multiSelectActive) toggleMulti.click();
         document.querySelectorAll("img[data-src]").forEach(img => img.click());
     }
+    function showTopSimp() {
+        var timeout = 0;
+        var profileUrl = document.querySelector('#menu-main .card-bottom a').href;
+
+        document.querySelectorAll(".selectCard").forEach((card) => {
+            timeout += 2000;
+            const cardid = $(card).data().card.id;
+
+            window.setTimeout(() => {
+                $.get("https://waifugame.com/json/card/" + cardid).then((responseJSON) => {
+                    var cid = responseJSON.characterId;
+                    var curl = "https://waifugame.com/search?q=ch:" + cid;
+
+                    card.href = curl;
+
+                    $.get(curl).then((charHtml) => {
+                        const parser = new DOMParser();
+                        const doc = parser.parseFromString(charHtml, 'text/html');
+                        let topSimpString = '<span>Top Simp: ';
+
+                        if (doc.querySelectorAll('table')[1].querySelector('tbody tr a').href === profileUrl) {
+                            topSimpString += '<b style="color: green!important">Yes</b>';
+                        } else {
+                            topSimpString += '<b style="color: red!important">No</b>';
+                        }
+                        topSimpString += '</span>';
+                        card.querySelector("span").insertAdjacentHTML('beforeend', topSimpString);
+                    });
+                });
+            }, timeout);
+        })
+    }
+    if (location.href.includes('box=1')) {
+        document.querySelector("#cardActionBlock")?.insertAdjacentHTML('afterend', "<button id='showTopSimp' style='margin: 10px'>Show Top Simp</button>");
+        document.getElementById('showTopSimp').addEventListener('click', showTopSimp);
+    }
     document.querySelector("#cardActionBlock")?.insertAdjacentHTML('afterend', "<button id='showStock' style='margin: 10px'>Show Stock</button>");
     document.getElementById('showStock').addEventListener('click', showStock);
     document.querySelector("#cardActionBlock")?.insertAdjacentHTML('afterend', "<button id='selectAllCards' style='margin: 10px'>Select all</button>");
@@ -64,7 +100,7 @@
         el.addEventListener("contextmenu", (e) => {
             showCardInfoMenuLookup(JSON.parse(el.dataset.card).id)
             e.preventDefault();
-        })
+        });
     });
 
     // let sum, max;
